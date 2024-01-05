a44fafeecbe7976b96416243935daf9f3b31ac31
lgw2238 <nihao3634@naver.com>
2024-01-05 10:25:04 +0900

    firebase 로그아웃

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,12 @@ import { Link } from "react-router-dom";
 
 
 export default function FooterComponent (){
-    return (
-        <header className="footer">
-            <div>
-                <Link to="/post/new"> 글쓰기 </Link>
-                <Link to="/posts"> 게시글  </Link>
-                <Link to="/profile"> 프로필  </Link>
-            </div>
-        </header>
-
-    );
+    
+  return (
+    <footer>
+      <Link to="/posts/new">글쓰기</Link>
+      <Link to="/posts">게시글</Link>
+      <Link to="/profile">프로필</Link>
+    </footer>
+  );
 }
